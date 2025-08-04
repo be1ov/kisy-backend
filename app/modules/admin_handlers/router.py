514b7a06b870f45265f919452545e6f869417504
vetrojet <slavik.pro0101@gmail.com>
2025-08-04 16:13:10 +0300

    fix orders to excel

--- app/modules/admin_handlers/router.py
+++ app/modules/admin_handlers/router.py
@@ -8,8 +8,8 @@ from app.modules.admin_handlers.service import ExcelService
 router = APIRouter()
 
 @router.get("/get_excel_orders")
-async def get_excel_orders(start_date: datetime = Depends(),
-    end_date: datetime = Depends(),
+async def get_excel_orders(start_date: datetime,
+    end_date: datetime,
     service:ExcelService = Depends()):
 
     try:
