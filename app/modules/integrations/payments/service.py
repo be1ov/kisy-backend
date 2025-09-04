6882aa2258f764e8f6ff80419cdfb0cec00a13c7
vetrojet <slavik.pro0101@gmail.com>
2025-09-04 18:18:56 +0300

    fix

--- app/modules/integrations/payments/service.py
+++ app/modules/integrations/payments/service.py
@@ -19,7 +19,7 @@ class PaymentIntegrationService:
         self.db = db
 
     async def process_payment(self, method: str, body):
-        payment_method_service = self.payments_service.get_payment_method(PaymentMethods[method])
+        payment_method_service = self.payments_service.get_payment_method(PaymentMethods(method))
         payment_id = await payment_method_service.process_payment(body)
 
         stmt = select(PaymentEntity).where(PaymentEntity.id == payment_id)
