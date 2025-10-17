4039e4e4eb6a37d491cedc3f3510d352a9be3e21
be1ov <b.e.1.o.v@yandex.ru>
2025-10-17 06:00:56 +0300

    feat

--- app/modules/goods/schemas/create.py
+++ app/modules/goods/schemas/create.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 
 
 class CreateGoodSchema(BaseModel):
-    id: tp.Optional[str]
+    id: tp.Optional[str] = None
     title: str
     description: str
     show_in_catalog: tp.Optional[bool] = True
