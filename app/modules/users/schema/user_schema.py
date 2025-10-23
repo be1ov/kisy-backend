882ba745b8417856dd93eb84d5f0069222abdd8b
be1ov <b.e.1.o.v@yandex.ru>
2025-10-23 05:44:14 +0300

    fix

--- app/modules/users/schema/user_schema.py
+++ app/modules/users/schema/user_schema.py
@@ -1,14 +1,15 @@
+import typing as tp
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class UserSchema(BaseModel):
     id: str
     first_name: str
     last_name: str
-    email: str
-    phone: str
-    birth_date: datetime | None = None
+    email: tp.Optional[str] = Field(None)
+    phone: tp.Optional[str] = Field(None)
+    birth_date: tp.Optional[datetime] = Field(None)
     telegram_id: int
     signup_completed: bool
     is_admin: bool = False
